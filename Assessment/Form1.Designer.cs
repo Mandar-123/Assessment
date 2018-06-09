61355a5318fa53cc3c316c2c959a72c0748947e5
Mandar Deo <mandardeo1997@gmail.com>
2018-06-09 13:44:05 +0530

    Initial Commit

---
+++ Assessment/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Assessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.newPanel = new System.Windows.Forms.Panel();
+            this.addNew = new System.Windows.Forms.Button();
+            this.new_tchkd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.new_alloc = new System.Windows.Forms.TextBox();
+            this.new_chkd = new System.Windows.Forms.TextBox();
+            this.new_mod = new System.Windows.Forms.TextBox();
+            this.new_ass = new System.Windows.Forms.TextBox();
+            this.save_but = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.newPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.OldLace;
+            this.panel1.Controls.Add(this.newPanel);
+            this.panel1.Location = new System.Drawing.Point(75, 49);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1216, 648);
+            this.panel1.TabIndex = 0;
+            // 
+            // newPanel
+            // 
+            this.newPanel.Controls.Add(this.addNew);
+            this.newPanel.Controls.Add(this.new_tchkd);
+            this.newPanel.Controls.Add(this.label1);
+            this.newPanel.Controls.Add(this.new_alloc);
+            this.newPanel.Controls.Add(this.new_chkd);
+            this.newPanel.Controls.Add(this.new_mod);
+            this.newPanel.Controls.Add(this.new_ass);
+            this.newPanel.Controls.Add(this.save_but);
+            this.newPanel.Location = new System.Drawing.Point(0, 329);
+            this.newPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.newPanel.Name = "newPanel";
+            this.newPanel.Size = new System.Drawing.Size(1084, 118);
+            this.newPanel.TabIndex = 1;
+            // 
+            // addNew
+            // 
+            this.addNew.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.addNew.Location = new System.Drawing.Point(1035, 0);
+            this.addNew.Name = "addNew";
+            this.addNew.Size = new System.Drawing.Size(50, 28);
+            this.addNew.TabIndex = 7;
+            this.addNew.Text = "Add";
+            this.addNew.UseVisualStyleBackColor = false;
+            this.addNew.Click += new System.EventHandler(this.addNew_Click);
+            // 
+            // new_tchkd
+            // 
+            this.new_tchkd.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_tchkd.Location = new System.Drawing.Point(955, 0);
+            this.new_tchkd.Name = "new_tchkd";
+            this.new_tchkd.Size = new System.Drawing.Size(50, 24);
+            this.new_tchkd.TabIndex = 6;
+            this.new_tchkd.TextChanged += new System.EventHandler(this.new_tchkd_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(769, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "/";
+            // 
+            // new_alloc
+            // 
+            this.new_alloc.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_alloc.Location = new System.Drawing.Point(827, 0);
+            this.new_alloc.Name = "new_alloc";
+            this.new_alloc.Size = new System.Drawing.Size(50, 24);
+            this.new_alloc.TabIndex = 4;
+            this.new_alloc.TextChanged += new System.EventHandler(this.new_alloc_TextChanged);
+            // 
+            // new_chkd
+            // 
+            this.new_chkd.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_chkd.Location = new System.Drawing.Point(646, 0);
+            this.new_chkd.Margin = new System.Windows.Forms.Padding(0);
+            this.new_chkd.Name = "new_chkd";
+            this.new_chkd.Size = new System.Drawing.Size(50, 24);
+            this.new_chkd.TabIndex = 3;
+            this.new_chkd.TextChanged += new System.EventHandler(this.new_chkd_TextChanged);
+            // 
+            // new_mod
+            // 
+            this.new_mod.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_mod.Location = new System.Drawing.Point(348, 0);
+            this.new_mod.Margin = new System.Windows.Forms.Padding(0);
+            this.new_mod.Name = "new_mod";
+            this.new_mod.Size = new System.Drawing.Size(250, 24);
+            this.new_mod.TabIndex = 2;
+            // 
+            // new_ass
+            // 
+            this.new_ass.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_ass.Location = new System.Drawing.Point(50, 0);
+            this.new_ass.Margin = new System.Windows.Forms.Padding(0);
+            this.new_ass.Name = "new_ass";
+            this.new_ass.Size = new System.Drawing.Size(250, 24);
+            this.new_ass.TabIndex = 1;
+            // 
+            // save_but
+            // 
+            this.save_but.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.save_but.Location = new System.Drawing.Point(556, 75);
+            this.save_but.Name = "save_but";
+            this.save_but.Size = new System.Drawing.Size(100, 40);
+            this.save_but.TabIndex = 0;
+            this.save_but.Text = "Save";
+            this.save_but.UseVisualStyleBackColor = false;
+            this.save_but.Click += new System.EventHandler(this.save_but_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.newPanel.ResumeLayout(false);
+            this.newPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button save_but;
+        private System.Windows.Forms.Panel newPanel;
+        private System.Windows.Forms.TextBox new_ass;
+        private System.Windows.Forms.TextBox new_mod;
+        private System.Windows.Forms.TextBox new_chkd;
+        private System.Windows.Forms.TextBox new_alloc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox new_tchkd;
+        private System.Windows.Forms.Button addNew;
+    }
+}
+
---
+++ Assessment/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace Assessment
+{
+    public partial class Form1 : Form
+    {
+        int sid = 1;
+        int total_entries = 1;
+        int txtBoxStartPosition = 50;
+        int txtBoxStartPositionV = 25;
+        int d = 48;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition, txtBoxStartPositionV, 250, "Assessor"));
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition + 250 + d, txtBoxStartPositionV, 250, "Moderator"));
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition + 490 + 2 * d, txtBoxStartPositionV, 75, "Checked"));
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition + 575 + 3 * d, txtBoxStartPositionV, 10, "/"));
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition + 575 + 4 * d, txtBoxStartPositionV, 75, "Allocated"));
+            panel1.Controls.Add(makeLabel(txtBoxStartPosition + 650 + 5 * d, txtBoxStartPositionV, 120, "Checked Today"));
+            txtBoxStartPositionV += 30;
+            load();
+        }
+
+        public static TextBox makeBox(int xLoc, int yLoc, int xSize, string t,string name, bool enabled, bool isNumber)
+        {
+            TextBox newText = new TextBox();
+            newText.ReadOnly = !enabled;
+            newText.Name = name;
+            newText.Location = new System.Drawing.Point(xLoc, yLoc);
+            newText.Size = new System.Drawing.Size(xSize, 30);
+            newText.Font = new Font("Arial", 11);
+            newText.Text = t;
+            if (isNumber == true)
+            {
+                newText.TextChanged += (s, e) =>
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(newText.Text, "[^0-9]"))
+                    {
+                        MessageBox.Show("Please enter only numbers.");
+                        newText.Text = newText.Text.Remove(newText.Text.Length - 1);
+                    }
+                };
+            }
+            return newText;
+        }
+
+        public static Label makeLabel(int xLoc, int yLoc, int xSize, string t)
+        {
+            Label newLabel = new Label();
+            newLabel.Location = new System.Drawing.Point(xLoc, yLoc);
+            newLabel.Size = new System.Drawing.Size(xSize, 30);
+            newLabel.Font = new Font("Arial", 11);
+            newLabel.Text = t;
+            return newLabel;
+        }
+
+        private void save_but_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < total_entries; i++)
+            {
+                string ass, mod;
+                int alloc, chkd, tchkd;
+                TextBox txtBox = panel1.Controls["ass_" + i.ToString()] as TextBox;
+                ass = txtBox.Text;
+                txtBox = panel1.Controls["mod_" + i.ToString()] as TextBox;
+                mod = txtBox.Text;
+                txtBox = panel1.Controls["chkd_" + i.ToString()] as TextBox;
+                chkd = Int32.Parse(txtBox.Text);
+                txtBox = panel1.Controls["tchkd_" + i.ToString()] as TextBox;
+                if (txtBox.Text == "")
+                    tchkd = 0;
+                else
+                    tchkd = Int32.Parse(txtBox.Text);
+                txtBox = panel1.Controls["alloc_" + i.ToString()] as TextBox;
+                if (txtBox.Text == "")
+                    alloc = 0;
+                else
+                    alloc = Int32.Parse(txtBox.Text);
+
+                string mDbPath = Application.StartupPath + "/paperassessment.db";
+                if (!File.Exists(mDbPath))
+                {
+                    SQLiteConnection.CreateFile(mDbPath);
+                }
+
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + mDbPath + ";Version=3;");
+                m_dbConnection.Open();
+                string sql;
+                SQLiteCommand command;
+                sql = "SELECT todayChecked FROM allocation WHERE sid = " + sid + " AND id = " + i + ";";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                int actual_add = 0;
+                while (reader.Read())
+                    actual_add = tchkd - (int)reader["todayChecked"];
+                chkd = chkd + actual_add;
+                sql = "UPDATE allocation SET assessor = '" + ass + "', moderator = '" + mod + "', allocated = " + alloc + ", checked = " + chkd + ", todayChecked = " + tchkd + " WHERE sid =" + sid + " AND id = " + i + ";";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+                txtBox = panel1.Controls["chkd_" + i.ToString()] as TextBox;
+                txtBox.Text = chkd.ToString();
+            }
+        }
+
+        private void new_chkd_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(new_chkd.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                new_chkd.Text = new_chkd.Text.Remove(new_chkd.Text.Length - 1);
+            }
+        }
+
+        private void new_alloc_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(new_alloc.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                new_alloc.Text = new_alloc.Text.Remove(new_alloc.Text.Length - 1);
+            }
+        }
+
+        private void new_tchkd_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(new_tchkd.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                new_tchkd.Text = new_tchkd.Text.Remove(new_tchkd.Text.Length - 1);
+            }
+        }
+
+        private void addNew_Click(object sender, EventArgs e)
+        {
+            string mDbPath = Application.StartupPath + "/paperassessment.db";
+            if (!File.Exists(mDbPath))
+            {
+                SQLiteConnection.CreateFile(mDbPath);
+            }
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + mDbPath + ";Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "CREATE TABLE IF NOT EXISTS allocation (sid int, id int, assessor varchar(50), moderator varchar(50), allocated int, checked int, todayChecked int);";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            string ass, mod;
+            int alloc, chkd, tchkd;
+            TextBox txtBox = newPanel.Controls["new_ass"] as TextBox;
+            ass = txtBox.Text;
+            txtBox = newPanel.Controls["new_mod"] as TextBox;
+            mod = txtBox.Text;
+            txtBox = newPanel.Controls["new_chkd"] as TextBox;
+            if (txtBox.Text == "")
+                chkd = 0;
+            else
+                chkd = Int32.Parse(txtBox.Text);
+            txtBox = newPanel.Controls["new_tchkd"] as TextBox;
+            if (txtBox.Text == "")
+                tchkd = 0;
+            else
+                tchkd = Int32.Parse(txtBox.Text);
+            txtBox = newPanel.Controls["new_alloc"] as TextBox;
+            if (txtBox.Text == "")
+                alloc = 0;
+            else
+                alloc = Int32.Parse(txtBox.Text);
+
+
+            sql = "INSERT into allocation (sid, id, assessor, moderator, allocated, checked, todayChecked) values (" + sid + ", " + total_entries + ", '" + ass + "', '" + mod + "', " + alloc + ", " + chkd + ", " + tchkd + ");";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            m_dbConnection.Close();
+            load();
+
+            new_ass.Text = "";
+            new_mod.Text = "";
+            new_chkd.Text = "";
+            new_tchkd.Text = "";
+            new_alloc.Text = "";
+        }
+
+        public void load() {
+
+            string mDbPath = Application.StartupPath + "/paperassessment.db";
+            if (!File.Exists(mDbPath))
+            {
+                SQLiteConnection.CreateFile(mDbPath);
+            }
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + mDbPath + ";Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "CREATE TABLE IF NOT EXISTS allocation (sid int, id int, assessor varchar(50), moderator varchar(50), allocated int, checked int, todayChecked int);";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "SELECT * FROM allocation where sid = " + sid + " AND id >= " + total_entries + " ORDER BY id";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                int t_id = (int)reader["id"];
+                string t_ass = (string)reader["assessor"];
+                string t_mod = (string)reader["moderator"];
+                int t_all = (int)reader["allocated"];
+                int t_chkd = (int)reader["checked"];
+                int t_tchkd = (int)reader["todayChecked"];
+                panel1.Controls.Add(makeBox(txtBoxStartPosition, txtBoxStartPositionV, 250, t_ass, "ass_" + t_id.ToString(), true, false));
+                panel1.Controls.Add(makeBox(txtBoxStartPosition + 250 + d, txtBoxStartPositionV, 250, t_mod, "mod_" + t_id.ToString(), true, false));
+                panel1.Controls.Add(makeBox(txtBoxStartPosition + 500 + 2 * d, txtBoxStartPositionV, 50, t_chkd.ToString(), "chkd_" + t_id.ToString(), false, true));
+                panel1.Controls.Add(makeLabel(txtBoxStartPosition + 575 + 3 * d, txtBoxStartPositionV, 10, "/"));
+                panel1.Controls.Add(makeBox(txtBoxStartPosition + 585 + 4 * d, txtBoxStartPositionV, 50, t_all.ToString(), "alloc_" + t_id.ToString(), true, true));
+                panel1.Controls.Add(makeBox(txtBoxStartPosition + 665 + 5 * d, txtBoxStartPositionV, 50, t_tchkd.ToString(), "tchkd_" + t_id.ToString(), true, true));
+                txtBoxStartPositionV += 30;
+                total_entries++;
+            }
+
+            newPanel.Location = new Point(0, txtBoxStartPositionV);
+        }
+    }
+}
